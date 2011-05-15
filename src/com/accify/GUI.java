7361e2dbdb99ad2ea871e71cff8d639ba652420a
Lorenzo Urbini <lorenzo.urbini@edgeint.net>
2011-05-15 18:25:51 +0100

    added label list with refresh but still list repetition

--- src/com/accify/GUI.java
+++ src/com/accify/GUI.java
@@ -5,13 +5,12 @@ import java.util.ArrayList;
 import java.awt.event.*;
 
 public class GUI {
-	
 	protected Frame f;
 	protected Panel ptop1;
-	protected Panel pbott1;
 	protected Panel ptop2;
 	protected Panel ptop3;
 	protected Panel ptop4;
+	protected Panel pbott1;
 	protected Panel pfooter;
 	protected TextArea tconsole;
 	protected TextArea tseconda;
@@ -21,7 +20,7 @@ public class GUI {
 	protected Label addlabel_attr;
 	protected Label addlabel_comp;
 	protected Label addlabel_capab;
-	protected List lista;
+	protected ArrayList<Label> lista;
 	protected MenuBar mb;
 	protected MenuItem m1;
 	protected MenuItem m2;
@@ -34,34 +33,42 @@ public class GUI {
 		gui.run(args);
 	}
 	
-	public void run (String [] args)
+	public void init()
+	{
+		buildMainGUI();
+		buildMenu();
+	}
+	
+	private void buildMainGUI() 
 	{
 		f = new Frame("Accify");
 		ptop1 = new Panel();
+		ptop2 = new Panel();
+	    ptop3 = new Panel();
+	    ptop4 = new Panel();
 		pbott1 = new Panel();
 		pfooter = new Panel();
-		lista = new List();
-		tconsole = new TextArea(1,70);
+		lista = new ArrayList<Label>();
+		tconsole = new TextArea(3,30);
 		addlabel_proj = new Label("Project Details");
 	    addlabel_attr = new Label("Attributes");
 	    addlabel_comp = new Label("Components");
 	    addlabel_capab = new Label("Capabilities");
-	    ptop2 = new Panel();
-	    ptop3 = new Panel();
-	    ptop4 = new Panel();
-	    ptop1.add(addlabel_proj);
+	    pbott1.add(addlabel_proj);
 		ptop2.add(addlabel_attr);
 		ptop3.add(addlabel_comp);
 		ptop4.add(addlabel_capab);
-		
-	    
-	    tf = new TextField(30);
+		tf = new TextField(30);
 		addlabel = new Button("Add Label");
-		tconsole.append("Console ready.");
 		pbott1.add(tf);
 		pbott1.add(addlabel);
+		f.add(pbott1);
 		pfooter.add(tconsole);
-		
+		f.add(pfooter);
+	}
+	
+	private void buildMenu() 
+	{
 		Menu menu = new Menu("File");
 		MenuItem m1 = new MenuItem("Project Details");
 		MenuItem m2 = new MenuItem("Attributes");
@@ -78,18 +85,21 @@ public class GUI {
 	    m2.addActionListener(new MenuListener());
 	    m3.addActionListener(new MenuListener());
 	    m4.addActionListener(new MenuListener());
-	  	    		
-		try {ptop1.add(new Label("Project name: "+ProvaDB.readProjectName()));} 
-		catch (HeadlessException e1) {e1.printStackTrace();} 
-		catch (Exception e1) {e1.printStackTrace();}
+	}
+	
+	public void run (String [] args)
+	{
+		init();
+		
+		try {pbott1.add(new Label("Project name: "+ProvaDB.readProjectName()));} catch (HeadlessException e1) {e1.printStackTrace();} catch (Exception e1) {e1.printStackTrace();}
+		
+		loadLista();
 		
 		addlabel.addActionListener(
 				new ActionListener() {
             public void actionPerformed(ActionEvent e){
             	AddLabel(e);
             }});
-		
-		refreshLista();
 		
 		f.addWindowListener(new WindowAdapter(){
             public void windowClosing(WindowEvent we)
@@ -98,29 +108,34 @@ public class GUI {
             }
         });
 		
-		f.add(ptop1);
-		f.add(pbott1);
-		f.add(pfooter);
 		
-		f.setSize(800, 600);
-		f.setVisible(true);
-		f.setLayout(new FlowLayout());
 	}
 
-	protected void refreshLista(){
-		ptop1.remove(lista);
+	protected void loadLista()
+	{
 		ArrayList<String> projectlabel = new ArrayList<String>();
 		try 
 		{
 			projectlabel = ProvaDB.readProjectLabels();
 		} 
 		catch (Exception e1) {e1.printStackTrace();}
-		lista.removeAll();
+		
 		for(String str:projectlabel)
 		{	
-			lista.add(str);
+			Label label_app = new Label(str);
+			lista.add(label_app);
 		}
-		ptop1.add(lista);
+		f.remove(ptop1);
+	
+		f.add(ptop1);
+		for(Label label_add:lista)
+		{
+			ptop1.add(label_add);
+		}
+		
+		f.setSize(1000,1000);
+		f.setVisible(true);
+		f.setLayout(new FlowLayout());
 	}
 	
 	protected void AddLabel(ActionEvent e)
@@ -140,36 +155,60 @@ public class GUI {
     	}
     	try  {ProvaDB.addLabel(field.getText(), projectname); } catch (Exception exc){System.err.println(exc);}
     	tconsole.append("\nLabel Added.");
-		refreshLista();
+		loadLista();
 	}
 	
-	class MenuListener implements ActionListener
+	protected class MenuListener implements ActionListener 
 	{
 		public void actionPerformed(ActionEvent e){
 			String item = e.getActionCommand(); 
 		    if (item.equals("Attributes"))  {
-			f.removeAll();
-			f.add(ptop2);
-			} else if (item.equals("Components")) {
-				f.removeAll();
-				f.add(ptop3);
-			} else if (item.equals("Capabilities")) {
-				f.removeAll();
-				f.add(ptop4);
-			} else if (item.equals("Project Info")) {
-				f.removeAll();
-				f.add(ptop1);
-				f.add(pbott1);
-			}
-		    
-		    f.add(pfooter);
-			f.setSize(800, 600);
+		    	buildm2GUI();
+			} 	else if (item.equals("Components")) {
+					buildm3GUI();				
+					} 	else if (item.equals("Capabilities")) {
+							buildm4GUI();				
+						} 	else if (item.equals("Project Details")) {
+								buildm1GUI();
+							}
+
+			f.setSize(1000, 1000);
 			f.setVisible(true);
 			f.setLayout(new FlowLayout());
-			tconsole.append("\nAttributes clicked");
-
+		}
+		
+		private void buildm3GUI() 
+		{
+			f.removeAll();
+			f.add(ptop3);
+			f.add(pfooter);
+		    tconsole.append("\nComponents clicked");
 		}
 
+		private void buildm2GUI() 
+		{
+			f.removeAll();
+			f.add(ptop2);
+		    f.add(pfooter);
+		    tconsole.append("\nAttributes clicked");
+		}
+
+		private void buildm4GUI() 
+		{
+			f.removeAll();
+			f.add(ptop4);
+			f.add(pfooter);
+		    tconsole.append("\nCapabilities clicked");			
+		}
+
+		private void buildm1GUI()
+		{
+			f.removeAll();
+			f.add(ptop1);
+			f.add(pbott1);
+			f.add(pfooter);
+		    tconsole.append("\nProject Details clicked");
+		}
 	}
 
 	
